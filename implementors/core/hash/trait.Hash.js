43fcf9843c51ef5c94a86cc94a80b03157f29845
Alex6614 <alexma6614@gmail.com>
2016-04-20 16:00:45 -0400

    Second Milestone

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['main'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='main/structs/struct.State.html' title='main::structs::State'>State</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='main/structs/struct.Key.html' title='main::structs::Key'>Key</a>&lt;'a&gt;",];
+implementors['main'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='main/structs/struct.State.html' title='main::structs::State'>State</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='main/structs/struct.Key.html' title='main::structs::Key'>Key</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
